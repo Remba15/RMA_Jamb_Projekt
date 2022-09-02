2a4ec5bc26585dfbc081ccf3c5496bd4bbd0adf3
Remba15 <renatokuna01@gmail.com>
2022-09-02 13:25:56 +0200

    -Dodant tekst za pravila igre, na fragment postavljen kao scrollable textView

--- app/src/main/java/com/example/yambprojekt/activities/InfoDialogFragment.kt
+++ app/src/main/java/com/example/yambprojekt/activities/InfoDialogFragment.kt
@@ -1,14 +1,20 @@
 package com.example.yambprojekt.activities
 
 import android.os.Bundle
+import android.os.ParcelFileDescriptor.open
+import android.text.method.ScrollingMovementMethod
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.TextView
 import androidx.fragment.app.DialogFragment
 import com.example.yambprojekt.R
+import java.io.*
+
 
 class InfoDialogFragment: DialogFragment(){
+    lateinit var mInfoText: TextView
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -20,6 +26,27 @@ class InfoDialogFragment: DialogFragment(){
             dismiss()
         }
 
+        mInfoText = rootView.findViewById(R.id.info_text)
+        mInfoText.movementMethod = ScrollingMovementMethod()
+        readTextFile()
+
         return rootView
+    }
+
+    fun readTextFile(){
+        val mInputStream: InputStream
+        val mOutputString: String
+
+        try{
+            mInputStream = this.resources.openRawResource(R.raw.ruleset)
+            val size: Int = mInputStream.available()
+            val buffer = ByteArray(size)
+            mInputStream.read(buffer)
+            mOutputString = String(buffer)
+            mInfoText.text = mOutputString
+        } catch (e: IOException){
+            e.printStackTrace()
+        }
+
     }
 }
